8f312e5249a471d02c674059d4f2bffb176f1589
WaleedShalal <waleed.elbana2011@gmail.com>
2021-11-15 14:16:48 +0200

    restructuring

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import logo from './logo.svg';
 import './App.scss';
+import Home from './components/Pages/Home/Home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <span className="btn btn-primary"><i class="fas fa-thumbs-up"></i></span>
+    <div className='App'>
+      <header className='App-header'>
+        <Home />
+        <span className='btn btn-primary'>
+          <i class='fas fa-thumbs-up'></i>
+        </span>
       </header>
     </div>
   );
