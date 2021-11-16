8cc59f168b22d07f0f87a0424ab8b0282c7f2757
WaleedShalal <waleed.elbana2011@gmail.com>
2021-11-16 20:24:58 +0200

    add admin component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.scss';
+import Admin from './components/Pages/Admin/Admin';
 import Products from './components/Pages/Products/Products';
 // import Home from './components/Pages/Home/Home';
 import Header from './components/Parts/Header/Header';
@@ -9,6 +10,7 @@ function App() {
       <Header />
       {/* <Home /> */}
       <Products />
+      <Admin />
     </div>
   );
 }
