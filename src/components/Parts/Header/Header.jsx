8687f8b8c41ef66c09a9af761412f679f464f818
WaleedShalal <waleed.elbana2011@gmail.com>
2021-11-17 23:13:17 +0200

    fix header component bug

--- src/components/Parts/Header/Header.jsx
+++ src/components/Parts/Header/Header.jsx
@@ -1,115 +1,144 @@
-import React from "react";
-import { NavLink ,Link ,useLocation } from "react-router-dom";
-import "./Header.scss";
+import React from 'react';
+import { NavLink, Link, useLocation } from 'react-router-dom';
+import './Header.scss';
 
 function Header(props) {
   let location = useLocation();
   console.log(location.pathname);
   return (
-    <div className="header">
-      <div className="container-fluid">
-        <header className="header-navbar">
-          <div className="leftPart">
-            <div className="logo">
-              <Link to='/'>
-              minuteف
-              </Link>
+    <div className='header'>
+      <div className='container-fluid'>
+        <header className='header-navbar'>
+          <div className='leftPart'>
+            <div className='logo'>
+              <Link to='/'>minuteف</Link>
             </div>
-            <div className="search-container">
-              <form className="search">
-                <div className="form-content">
-                  <span className="search-icon">
-                    <i className="fas fa-search"></i>
+            <div className='search-container'>
+              <form className='search'>
+                <div className='form-content'>
+                  <span className='search-icon'>
+                    <i className='fas fa-search'></i>
                   </span>
                   <input
-                    type="text"
-                    name="search"
-                    aria-label="Search"
-                    className="search-input"
-                    placeholder="Search"
-                  ></input>
+                    type='text'
+                    name='search'
+                    aria-label='Search'
+                    className='search-input'
+                    placeholder='Search'></input>
                 </div>
               </form>
             </div>
           </div>
-          <div className="rightPart">
-            <div className="RighC">
-              <NavLink 
-                style={({ isActive }) => {
-                  return {
-                    display: "block",
-                    margin: "1rem 0",
-                    color: isActive ? "white" : "rgba(255, 255, 255, 0.65)",
-                  };
-                }}
-                to={`/`}
-              >
-                <i className="fas fa-home "></i>
-              </NavLink>
-            </div>
-            <div className="RighC">
-              <i className="fas fa-comment "></i>
-            </div>
-            <div className="RighC">
-              <i className="fas fa-bell "></i>
-            </div>
-            <div className="RighC">
+          <div className='rightPart'>
+            <div className='RighC'>
               <NavLink
                 style={({ isActive }) => {
                   return {
-                    display: "block",
-                    margin: "1rem 0",
-                    color: isActive ? "white" : "rgba(255, 255, 255, 0.65)",
+                    display: 'block',
+                    margin: '1rem 0',
+                    color: isActive ? 'white' : 'rgba(255, 255, 255, 0.65)',
                   };
                 }}
-                to={`/cart`}
-              >
-                <i class="fas fa-shopping-bag"></i>
-              </NavLink>
-            </div> 
-            <div className="RighC">
-              <NavLink
-                style={({ isActive }) => {
-                  return {
-                    display: "block",
-                    margin: "1rem 0",
-                    color: isActive ? "white" : "rgba(255, 255, 255, 0.65)",
-                  };
-                }}
-                to={`/profile`}
-              >
-                <i className="fas fa-user-circle"></i>
+                to={`/`}>
+                <i className='fas fa-home '></i>
               </NavLink>
             </div>
-           
-            <div className="RighC">
-            {location.pathname !== "/register" ? 
-          
+            <div className='RighC'>
+              <i className='fas fa-comment '></i>
+            </div>
+            <div className='RighC'>
+              <i className='fas fa-bell '></i>
+            </div>
+            <div className='RighC'>
               <NavLink
                 style={({ isActive }) => {
                   return {
-                    display: "block",
-                    margin: "1rem 0",
-                    color: isActive ? "white" : "rgba(255, 255, 255, 0.65)",
+                    display: 'block',
+                    margin: '1rem 0',
+                    color: isActive ? 'white' : 'rgba(255, 255, 255, 0.65)',
                   };
                 }}
-                to={`/register`}
-              >
-               Sign up <i className="fas fa-sign-out-alt ps-2"></i>
+                to={`/cart`}>
+                <i className='fas fa-shopping-bag'></i>
               </NavLink>
-              :<NavLink
-              style={({ isActive }) => {
-                return {
-                  display: "block",
-                  margin: "1rem 0",
-                  color: isActive ? "white" : "rgba(255, 255, 255, 0.65)",
-                };
-              }}
-              to={`/login`}
-            >
-              Sign in <i className="fas fa-sign-out-alt ps-2"></i>
-            </NavLink>
-                }
+            </div>
+            <div className='RighC'>
+              <NavLink
+                style={({ isActive }) => {
+                  return {
+                    display: 'block',
+                    margin: '1rem 0',
+                    color: isActive ? 'white' : 'rgba(255, 255, 255, 0.65)',
+                  };
+                }}
+                to={`/profile`}>
+                <i className='fas fa-user-circle'></i>
+              </NavLink>
+            </div>
+
+            <div className='RighC'>
+              {location.pathname === '/register' ? (
+                <NavLink
+                  style={({ isActive }) => {
+                    return {
+                      display: 'block',
+                      margin: '1rem 0',
+                      color: isActive ? 'white' : 'rgba(255, 255, 255, 0.65)',
+                    };
+                  }}
+                  to={`/login`}>
+                  Sign in <i className='fas fa-sign-out-alt ps-2'></i>
+                </NavLink>
+              ) : location.pathname === '/login' ? (
+                <NavLink
+                  style={({ isActive }) => {
+                    return {
+                      display: 'block',
+                      margin: '1rem 0',
+                      color: isActive ? 'white' : 'rgba(255, 255, 255, 0.65)',
+                    };
+                  }}
+                  to={`/register`}>
+                  Sign up <i className='fas fa-sign-out-alt ps-2'></i>
+                </NavLink>
+              ) : (
+                <NavLink
+                  style={({ isActive }) => {
+                    return {
+                      display: 'block',
+                      margin: '1rem 0',
+                      color: isActive ? 'white' : 'rgba(255, 255, 255, 0.65)',
+                    };
+                  }}
+                  to={`/login`}>
+                  Logout <i className='fas fa-sign-out-alt ps-2'></i>
+                </NavLink>
+              )}
+              {/* {location.pathname === '/register' ? (
+                <NavLink
+                  style={({ isActive }) => {
+                    return {
+                      display: 'block',
+                      margin: '1rem 0',
+                      color: isActive ? 'white' : 'rgba(255, 255, 255, 0.65)',
+                    };
+                  }}
+                  to={`/login`}>
+                  Sign in <i className='fas fa-sign-out-alt ps-2'></i>
+                </NavLink>
+              ) : (
+                <NavLink
+                  style={({ isActive }) => {
+                    return {
+                      display: 'block',
+                      margin: '1rem 0',
+                      color: isActive ? 'white' : 'rgba(255, 255, 255, 0.65)',
+                    };
+                  }}
+                  to={`/login`}>
+                  Sign in <i className='fas fa-sign-out-alt ps-2'></i>
+                </NavLink>
+              )} */}
             </div>
           </div>
         </header>
