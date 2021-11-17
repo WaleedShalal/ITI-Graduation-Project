a9c9d704a407b3d8209d1c31c6ae74670270a6c7
WaleedShalal <waleed.elbana2011@gmail.com>
2021-11-17 02:18:42 +0200

    add login component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-
 import Header from './components/Parts/Header/Header';
-import Home from './components/Pages/Home/Home';
-import Profile from './components/Pages/Profile/Profile';
+// import Home from './components/Pages/Home/Home';
+// import Profile from './components/Pages/Profile/Profile';
 
 import './App.scss';
+import Login from './components/Pages/Login/Login';
 function App() {
   return (
     <div className='App'>
       <Header />
-      <Profile />
+      {/* <Profile /> */}
+      <Login />
     </div>
   );
 }
