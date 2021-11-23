a093234dd59d4dc5ac760613228461f3a1dd4478
Mohamed-Dibo <diaboom650@gmail.com>
2021-11-23 16:27:02 +0200

    home design

---
+++ src/components/Parts/PeopleYouKnow/PeopleYouKnow.jsx
@@ -0,0 +1,63 @@
+import React from 'react'
+import './PeopleYouKnow.scss'
+function PeopleYouKnow() {
+    return (
+        <div class="widget stick-widget" >
+        <h4 class="widget-title">Who's follownig</h4>
+        <ul class="followers">
+            <li>
+                <figure><img alt="" src="https://via.placeholder.com/100" /></figure>
+                <div class="friend-meta">
+                    <h4>
+                        <a title="" href="time-line.html">Kelly Bill</a>
+                        <span>Dept colleague</span>
+                    </h4>
+                    <a class="underline" title="" href="#">Follow</a>
+                </div>
+            </li>
+            <li>
+                <figure><img alt="" src="https://via.placeholder.com/100" /></figure>
+                <div class="friend-meta">
+                    <h4>
+                        <a title="" href="time-line.html">Issabel</a>
+                        <span>Dept colleague</span>
+                    </h4>
+                    <a class="underline" title="" href="#">Follow</a>
+                </div>
+            </li>
+            <li>
+                <figure><img alt="" src="https://via.placeholder.com/100" /></figure>
+                <div class="friend-meta">
+                    <h4>
+                        <a title="" href="time-line.html">Andrew</a>
+                        <span>Dept colleague</span>
+                    </h4>
+                    <a class="underline" title="" href="#">Follow</a>
+                </div>
+            </li>
+            <li>
+                <figure><img alt="" src="https://via.placeholder.com/100" /></figure>
+                <div class="friend-meta">
+                    <h4>
+                        <a title="" href="time-line.html">Sophia</a>
+                        <span>Dept colleague</span>
+                    </h4>
+                    <a class="underline" title="" href="#">Follow</a>
+                </div>
+            </li>
+            <li>
+                <figure><img alt="" src="https://via.placeholder.com/100" /></figure>
+                <div class="friend-meta">
+                    <h4>
+                        <a title="" href="time-line.html">Allen</a>
+                        <span>Dept colleague</span>
+                    </h4>
+                    <a class="underline" title="" href="#">Follow</a>
+                </div>
+            </li>
+        </ul>	
+    </div>
+    )
+}
+
+export default PeopleYouKnow
--- src/components/Parts/Stars/Stars.jsx
+++ src/components/Parts/Stars/Stars.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
-
+import './Stars.scss'
 function Stars() {
     return (
-        <div className="me-2">
-        <i className="fas fa-star" style={{color: '#FEDE00'}}></i>
-        <i className="fas fa-star" style={{color: '#FEDE00'}}></i>
-        <i className="fas fa-star" style={{color: '#FEDE00'}}></i>
-        <i className="fas fa-star" style={{color: '#FEDE00'}}></i>
-        <i className="far fa-star"></i>
-      </div>
+      <ul class="stars mb-0">
+      <li><i class="fas fa-star"></i></li>
+      <li><i class="fas fa-star"></i></li>
+      <li><i class="fas fa-star"></i></li>
+      <li><i class="fas fa-star"></i></li>
+      <li><i class="fas fa-star"></i></li>
+    </ul>
     )
 }
 
