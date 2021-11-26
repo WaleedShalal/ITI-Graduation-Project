f95b0a2e68621959e4c7604e9e7254755827f31a
WaleedShalal <waleed.elbana2011@gmail.com>
2021-11-26 16:14:26 +0200

    edit navbar

--- src/components/Pages/Messages/MessagesView/Messages.jsx
+++ src/components/Pages/Messages/MessagesView/Messages.jsx
@@ -42,56 +42,79 @@ function Messages() {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [messages]);
   return (
-    <section className='messages my-3'>
-      <div className='container'>
-        <div className='messages__wrapper'>
-          <div className='row'>
-            <div className='messages__inbox col-4 pe-0'>
-              <div className='messages__inboxHeader d-flex justify-content-around align-items-center px-3'>
-                <h4>{userData?.displayName}</h4>
-              </div>
-              <div className='messages__inboxBody py-4'>
-                {users
-                  ?.filter((data) => data.userId !== userData.uid)
-                  ?.map((data, index) => (
-                    <MessagesUsers data={data} key={index} />
-                  ))}
-              </div>
-            </div>
-            {!secondUserData ? (
-              <div className='fs-1 col-8 ps-0 d-flex justify-content-center align-items-center'>
-                welcome to message
-              </div>
-            ) : (
-              <div className='messages__chat col-8 ps-0'>
-                <div className='messages__chatHeader d-flex justify-content-between align-items-center px-3'>
-                  <ChatUserHeader />
-                  <i className='fas fa-exclamation-circle'></i>
-                </div>
-                <div className='messages__chatBody px-3 py-4'>
-                  {messagesSorted.map((data) => (
-                    <ChatUserBody
-                      key={data.sentAt}
-                      isCurrent={data.sentBy === userData.uid}
-                      data={data.msg}
-                      time={data.sentAt}
-                      userPhoto={
-                        data.sentBy === userData.uid
-                          ? userData.photoURL
-                          : secondUserData.userPhoto
-                      }
-                    />
-                  ))}
-                </div>
-                <div className='messages__chatFooter d-flex justify-content-between align-items-center px-4 py-2'>
-                  <ChatUserFooter />
-                </div>
-              </div>
-            )}
-          </div>
+    <header>
+      <nav className='d-flex flex-row jutify-content-center py-2'>
+        <div>logo </div>
+        <form>
+          <input type='text' />
+          <button>search</button>
+        </form>
+        <div className='ms-auto'>
+          <h6>waleed elbana</h6>
         </div>
-      </div>
-    </section>
+        <input type='checkbox' id='check' />
+        <label htmlFor='check' className='ms-auto'>
+          <i className='fas fa-bars'></i>
+        </label>
+
+        <ul className='list-unstyled d-flex ms-auto'>
+          <li>home</li>
+          <li>home</li>
+          <li>home</li>
+          <li>home</li>
+        </ul>
+      </nav>
+    </header>
+    // <section className='messages my-3'>
+    //   <div className='container'>
+    //     <div className='messages__wrapper'>
+    //       <div className='row'>
+    //         <div className='messages__inbox col-4 pe-0'>
+    //           <div className='messages__inboxHeader d-flex justify-content-around align-items-center px-3'>
+    //             <h4>{userData?.displayName}</h4>
+    //           </div>
+    //           <div className='messages__inboxBody py-4'>
+    //             {users
+    //               ?.filter((data) => data.userId !== userData.uid)
+    //               ?.map((data, index) => (
+    //                 <MessagesUsers data={data} key={index} />
+    //               ))}
+    //           </div>
+    //         </div>
+    //         {!secondUserData ? (
+    //           <div className='fs-1 col-8 ps-0 d-flex justify-content-center align-items-center'>
+    //             welcome to message
+    //           </div>
+    //         ) : (
+    //           <div className='messages__chat col-8 ps-0'>
+    //             <div className='messages__chatHeader d-flex justify-content-between align-items-center px-3'>
+    //               <ChatUserHeader />
+    //               <i className='fas fa-exclamation-circle'></i>
+    //             </div>
+    //             <div className='messages__chatBody px-3 py-4'>
+    //               {messagesSorted.map((data) => (
+    //                 <ChatUserBody
+    //                   key={data.sentAt}
+    //                   isCurrent={data.sentBy === userData.uid}
+    //                   data={data.msg}
+    //                   time={data.sentAt}
+    //                   userPhoto={
+    //                     data.sentBy === userData.uid
+    //                       ? userData.photoURL
+    //                       : secondUserData.userPhoto
+    //                   }
+    //                 />
+    //               ))}
+    //             </div>
+    //             <div className='messages__chatFooter d-flex justify-content-between align-items-center px-4 py-2'>
+    //               <ChatUserFooter />
+    //             </div>
+    //           </div>
+    //         )}
+    //       </div>
+    //     </div>
+    //   </div>
+    // </section>
   );
 }
 
--- src/components/Parts/Header/Header.jsx
+++ src/components/Parts/Header/Header.jsx
@@ -27,6 +27,129 @@ function Header() {
       });
   }, [data]);
   return (
+    // <nav className='main__navbar navbar navbar-expand-lg navbar-light'>
+    //   <div className='container-fluid'>
+    //     <Link className='navbar__logo navbar-brand' to='/'>
+    //       Minuteف
+    //     </Link>
+
+    //     {/* <span className='navbar-toggler-icon'></span> */}
+    //     <div className='navbar__search'>
+    //       <form className='search__form d-flex'>
+    //         <input
+    //           className='search__formInput form-control'
+    //           type='search'
+    //           placeholder='Search...'
+    //           aria-label='Search'
+    //         />
+    //         <button className='search__formBtn' type='submit'>
+    //           <i className='fas fa-search text-dark '></i>
+    //         </button>
+    //       </form>
+    //     </div>
+
+    //     {user && (
+    //       <div className='ms-auto navbar__list'>
+    //         <div className='navbar__listItem  nav-item'>
+    //           <div className='list__itemProfile'>
+    //             <Link
+    //               className='nav-link d-flex align-items-center p-1'
+    //               aria-current='page'
+    //               to='/profile'>
+    //               <figure className='mb-0 rounded-circle'>
+    //                 <img
+    //                   className='w-100 rounded-circle'
+    //                   src={data.imageUrl ? data.imageUrl : image}
+    //                   alt=''
+    //                 />
+    //               </figure>
+    //               <div className='ms-1'>
+    //                 <h5 className='text-capitalize mb-0'>{user.displayName}</h5>
+    //               </div>
+    //             </Link>
+    //             <div className='list__itemProfileDecoration'></div>
+    //           </div>
+    //         </div>
+    //       </div>
+    //     )}
+    //     <ul className='navbar__list navbar-nav ms-0 mb-2 mb-lg-0 align-items-center'>
+    //       <li className='navbar__listItem nav-item'>
+    //         <Link
+    //           className='item__link nav-link active'
+    //           aria-current='page'
+    //           to='/'>
+    //           <i className='fa__item fas fa-home'></i>
+    //         </Link>
+    //       </li>
+    //       <li className='navbar__listItem nav-item'>
+    //         <Link className='item__link nav-link' to='/messages'>
+    //           <i className='fa__item far fa-comment-alt'></i>
+    //           <span className='item__notification'></span>
+    //         </Link>
+    //       </li>
+    //       <li className='item__forNotification navbar__listItem nav-item'>
+    //         <input type='checkbox' id='notification' />
+    //         <label
+    //           htmlFor='notification'
+    //           className='item__forNotification item__link nav-link'>
+    //           <i className='fa__item far fa-bell'></i>
+    //           <span className='item__notification'></span>
+    //         </label>
+    //         <Notifications />
+    //       </li>
+    //       <li className='navbar__listItem nav-item'>
+    //         <Link className=' item__link nav-link' to='/cart'>
+    //           <i className='fa__item fas fa-shopping-cart'>
+    //             {fetchedData.purchased.length}
+    //           </i>
+    //         </Link>
+    //       </li>
+    //       <li className='navbar__listItem nested__list  nav-item'>
+    //         <div className='item__link'>
+    //           <i className='fa__item fas fa-th-large'></i>
+    //         </div>
+    //         <ul className='list__itemOption list-unstyled text-dark p-2'>
+    //           <li className='option__eCommerce mb-2'>
+    //             <Link className='btn text-capitalize w-100' to='/products'>
+    //               <i className='fas fa-store'></i> ecommerce
+    //             </Link>
+    //           </li>
+    //           <li className='option__setting mb-2'>
+    //             <Link className='btn text-capitalize w-100' to='/edit'>
+    //               <i className='fas fa-cogs'></i> Edit Profile
+    //             </Link>
+    //           </li>
+    //           <li className='option__logout'>
+    //             {location.pathname === '/register' ? (
+    //               <NavLink
+    //                 className=' btn btn-outline-danger  text-capitalize w-100'
+    //                 to={`/login`}>
+    //                 <i className='fas fa-sign-out-alt'></i> Sign in
+    //               </NavLink>
+    //             ) : location.pathname === '/login' ||
+    //               location.pathname === '/wellcome' ? (
+    //               <NavLink
+    //                 className=' btn btn-outline-danger  text-capitalize w-100'
+    //                 to={`/register`}>
+    //                 <i className='fas fa-sign-out-alt'></i> Sign up
+    //               </NavLink>
+    //             ) : (
+    //               <NavLink
+    //                 className=' btn btn-outline-danger  text-capitalize w-100'
+    //                 to={`/wellcome`}
+    //                 onClick={() => auth.signOut()}>
+    //                 <i className='fas fa-sign-out-alt'></i> logout
+    //               </NavLink>
+    //             )}
+    //           </li>
+    //         </ul>
+    //       </li>
+    //     </ul>
+    //   </div>
+    // </nav>
+
+    /* ------------------------------- start edit ------------------------------- */
+
     <nav className='main__navbar navbar navbar-expand-lg navbar-light'>
       <div className='container-fluid'>
         <Link className='navbar__logo navbar-brand' to='/'>
