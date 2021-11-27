d98eb1d7dce64abc4e3c2f5875e337cfff9bc019
WaleedShalal <waleed.elbana2011@gmail.com>
2021-11-27 19:16:29 +0200

    fix pagination bug

--- src/components/Parts/Pagination/Pagination.jsx
+++ src/components/Parts/Pagination/Pagination.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 function Pagination({ dataPerPage, totalData, paginate }) {
   const pagesNumber = [];
-  for (let i = 1; i <= Math.floor(totalData / dataPerPage); i++) {
+  for (let i = 1; i <= Math.ceil(totalData / dataPerPage); i++) {
     pagesNumber.push(i);
   }
   return (
