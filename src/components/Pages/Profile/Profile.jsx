b7297b7238f549a2ddac96f04142f9a0b77936bf
Mohamed-Dibo <diaboom650@gmail.com>
2021-11-29 15:38:40 +0200

    profile edit

--- src/components/Pages/Profile/Profile.jsx
+++ src/components/Pages/Profile/Profile.jsx
@@ -3,14 +3,32 @@ import Post from '../../Parts/Post/Post';
 import Stars from '../../Parts/Stars/Stars';
 import { useContext, useEffect, useState } from 'react';
 import { AuthContext } from '../../../context/Auth';
-import { db } from '../.../../../../Firebase/Firebase';
+import { auth, db } from '../.../../../../Firebase/Firebase';
 import NoPosts from '../../Parts/Post/NoPosts';
 import Stories from '../../Parts/Stories/Stories';
 import ImageUpload from './../../Parts/VideoUpload/VideoUpload';
 import './Profile.scss';
 function Profile() {
+  const [image, setimage] = useState(
+    "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png"
+  );
+  const [data, setData] = useState({
+    imageUrl: image,
+  });
+
   const [posts, setPosts] = useState([]);
   const { user } = useContext(AuthContext);
+
+  useEffect(() => {
+    db.collection("users")
+      .doc(auth.currentUser?.uid)
+      .get()
+      .then((snapshot) => {
+        if (snapshot.exists) {
+          setData(snapshot.data());
+        }
+      });
+  }, [data]);
 
   useEffect(() => {
     db.collection('posts')
@@ -32,11 +50,11 @@ function Profile() {
   return (
     <div className='profile pt-5'>
       <div className='container'>
-        <div className='row'>
-          <div className='col-7'>
+        <div className='row flex-column-reverse flex-lg-row'>
+          <div className='col-lg-7 col-12'>
             <Stories />
             <ImageUpload />
-            <div className='content-page'>
+            <div className='content-page w-100'>
               {posts.length > 0 ? (
                 posts.map(({ id, post }) => {
                   return (
@@ -47,7 +65,7 @@ function Profile() {
                       video={post.videoUrl}
                       caption={post.caption}
                       rate={post.rate}
-                    />
+                    />  
                   );
                 })
               ) : (
@@ -55,13 +73,13 @@ function Profile() {
               )}
             </div>
           </div>
-          <div className='user-info offset-1 col-4 p-3'>
+          <div className='user-info offset-lg-1 col-lg-4 col-12 p-3'>
             <div className='d-flex flex-column justify-content-center align-items-center mb-5'>
               <h5 className='text-dark'>
                 {user.displayName.toUpperCase()}{' '}
                 <i className='user__badge far fa-id-badge'></i>
               </h5>
-              <img src='http://placehold.it/70' alt='profile' />
+              <img src={data.imageUrl ? data.imageUrl : image} alt='profile' />
               <div className='rate d-flex'>
                 <Stars />
                 <span className='rate-number text-dark'>5.0</span>
