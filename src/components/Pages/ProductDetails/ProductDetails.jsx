928dc0336d76c6d64901063bf14bc02fef8d8e53
WaleedShalal <waleed.elbana2011@gmail.com>
2021-12-11 02:21:18 +0200

    edit product details style

--- src/components/Pages/ProductDetails/ProductDetails.jsx
+++ src/components/Pages/ProductDetails/ProductDetails.jsx
@@ -1,41 +1,43 @@
-import React, { useEffect, useState } from "react";
-import { useSelector, useDispatch } from "react-redux";
-import { Link, useLocation, useNavigate, useParams } from "react-router-dom";
-import { addCart } from "../../../store/cartActions";
-import Review from "./Review";
-import { auth } from "../../../Firebase/Firebase";
-import "./ProductDetails.scss";
+import React, { useEffect, useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { Link, useLocation, useNavigate, useParams } from 'react-router-dom';
+import { addCart } from '../../../store/cartActions';
+import Review from './Review';
+import { auth } from '../../../Firebase/Firebase';
+import './ProductDetails.scss';
 
 const ProductDetails = () => {
-  const [comment, setcomment] = useState("");
-  const [rate, setRate] = useState("");
-  const [reviews, setReviews] = useState([{
-        userId: 123,
-        userName: "jhone Deo",
-        comment: "Awesome :)",
-        rate: 5,
-  }]);
+  const [comment, setcomment] = useState('');
+  const [rate, setRate] = useState('');
+  const [reviews, setReviews] = useState([
+    {
+      userId: 123,
+      userName: 'jhone Deo',
+      comment: 'Awesome :)',
+      rate: 5,
+    },
+  ]);
 
   const { id } = useParams();
   const { pathname } = useLocation();
   const navigate = useNavigate();
   const dispatch = useDispatch();
   const { fetchedData } = useSelector((state) => state);
-  const [productId,setProductId]= useState("")
-  const isFromCart = pathname.includes("cart");
+  const [productId, setProductId] = useState('');
+  const isFromCart = pathname.includes('cart');
   const productItem = fetchedData.products.filter(
-    (p) => p.id === parseInt(productId)
+    (p) => p.id === parseInt(productId),
   )[0];
   const purchasedItem = fetchedData.purchased.filter(
-    (p) => p.id === parseInt(productId)
+    (p) => p.id === parseInt(productId),
   )[0];
 
-  useEffect(()=>{
+  useEffect(() => {
     setProductId(id);
-  },[id,productId]);
+  }, [id, productId]);
 
   const handleBack = () => {
-    isFromCart ? navigate("/cart") : navigate("/products/1");
+    isFromCart ? navigate('/cart') : navigate('/products/1');
   };
   const addReview = (e) => {
     e.preventDefault();
@@ -48,8 +50,8 @@ const ProductDetails = () => {
         rate: rate,
       },
     ]);
-    setcomment("");
-    setRate("");
+    setcomment('');
+    setRate('');
   };
   const handleChangeComment = (e) => {
     setcomment(e.target.value);
@@ -59,77 +61,75 @@ const ProductDetails = () => {
   };
   return (
     <React.Fragment>
-      <section className="full-details mt-2">
-        <div className="product__details__wrapper container-fluid mt-5">
-          <div className="row ">
-            <div className="product__images col-6">
-              <div className="row align-items-center">
-                <div className="mini__images col-2">
+      <section className='full-details mt-2'>
+        <div className='product__details__wrapper container-fluid mt-5'>
+          <div className='row '>
+            <div className='product__images col-6'>
+              <div className='row align-items-center'>
+                <div className='mini__images col-2'>
                   <figure>
-                    <img src={productItem?.image} alt="" />
+                    <img className='w-100' src={productItem?.image} alt='' />
                   </figure>
                   <figure>
-                    <img src={productItem?.image} alt="" />
+                    <img className='w-100' src={productItem?.image} alt='' />
                   </figure>
                   <figure>
-                    <img src={productItem?.image} alt="" />
+                    <img className='w-100' src={productItem?.image} alt='' />
                   </figure>
                 </div>
-                <figure className="col-10 mb-0">
+                <figure className='col-10 mb-0'>
                   <img
-                    className="product-img w-100"
+                    className='product-img w-100'
                     src={productItem?.image}
-                    alt=""
+                    alt=''
                   />
                 </figure>
               </div>
             </div>
-            <div className="product-info offset-1 col-5 d-flex flex-column justify-content-between py-4">
-              <div className="info">
-                <div className="mb-3">
-                  <span className="fw-bold  text-capitalize">product id</span> :{" "}
+            <div className='product-info offset-1 col-5 d-flex flex-column justify-content-between py-4'>
+              <div className='info'>
+                <div className='mb-3'>
+                  <span className='fw-bold  text-capitalize'>product id</span> :{' '}
                   {productItem?.id}.
                 </div>
-                <div className="mb-3">
-                  <span className="fw-bold text-capitalize">product Title</span>{" "}
+                <div className='mb-3'>
+                  <span className='fw-bold text-capitalize'>product Title</span>{' '}
                   : {productItem?.title}.
                 </div>
-                <div className="mb-3">
-                  <span className="fw-bold text-capitalize">
+                <div className='mb-3'>
+                  <span className='fw-bold text-capitalize'>
                     product Desciption
-                  </span>{" "}
+                  </span>{' '}
                   : {productItem?.description}.
                 </div>
-                <div className="mb-3">
-                  <span className="fw-bold text-capitalize">product Price</span>{" "}
+                <div className='mb-3'>
+                  <span className='fw-bold text-capitalize'>product Price</span>{' '}
                   : {productItem?.price}$.
                 </div>
-                <div className="">
-                  <span className="fw-bold text-capitalize ">
+                <div className=''>
+                  <span className='fw-bold text-capitalize '>
                     product quantity in cart
-                  </span>{" "}
-                  :{" "}
-                  <span className="text-success fw-bold">
+                  </span>{' '}
+                  :{' '}
+                  <span className='text-success fw-bold'>
                     {purchasedItem ? purchasedItem.count : 0}.
                   </span>
                 </div>
               </div>
-              <div className="options__btn row pt-3">
-                <div className="col-sm-12 col-md-6 text-center p-1">
+              <div className='options__btn row pt-3'>
+                <div className='col-sm-12 col-md-6 text-center p-1'>
                   <button
                     onClick={() => dispatch(addCart(productItem))}
-                    className="option__addCart btn btn-primary"
-                  >
+                    className='option__addCart btn btn-primary'>
                     Add Cart
                   </button>
                 </div>
-                <div className="col-sm-12 col-md-6 text-center p-1">
+                <div className='col-sm-12 col-md-6 text-center p-1'>
                   <Link
-                    to="/products/1"
+                    to='/products/1'
                     onClick={handleBack}
-                    className="option__backProducts btn btn-outline-primary"
-                  >
-                    {isFromCart ? "Back Cart" : "Back Products"}
+                    className='option__backProducts btn btn-outline-primary'>
+                    {isFromCart ? 'Back Cart' : 'Back Products'}
                   </Link>
                 </div>
               </div>
@@ -137,45 +137,45 @@ const ProductDetails = () => {
           </div>
         </div>
       </section>
-      <section className="product__reviews">
-        <div className="container">
+      <section className='product__reviews'>
+        <div className='container'>
           <form onSubmit={addReview}>
-            <div className="row py-3">
-              <div className="input-container col-lg-8 col-10">
+            <div className='row py-3'>
+              <div className='input-container col-lg-8 col-10'>
                 <input
-                  id="review"
-                  className="input"
-                  type="text"
-                  pattern=".+"
+                  id='review'
+                  className='input'
+                  type='text'
+                  pattern='.+'
                   required
-                  name="review"
+                  name='review'
                   onChange={handleChangeComment}
                   value={comment}
                 />
-                <label className="label" htmlFor="review">
+                <label className='label' htmlFor='review'>
                   Add Your Review
                 </label>
               </div>
 
               <input
-                className="input-number col-lg-1 col-3 mt-lg-0 mt-3 me-2 ms-3 ms-lg-0 "
-                type="number"
-                max="5"
-                min="0"
-                placeholder="rate"
+                className='input-number col-lg-1 col-3 mt-lg-0 mt-3 me-2 ms-3 ms-lg-0 '
+                type='number'
+                max='5'
+                min='0'
+                placeholder='rate'
                 onChange={handleChangeRate}
                 value={rate}
               />
 
               <input
-                className="add_review col-lg-2 mt-3 mt-lg-0 col-3"
-                type="submit"
-                value="add review"
+                className='add_review col-lg-2 mt-3 mt-lg-0 col-3'
+                type='submit'
+                value='add review'
               />
             </div>
           </form>
-          <h2 className="text-capitalize mb-5">top reviews</h2>
-          <div className="row">
+          <h2 className='text-capitalize mb-5'>top reviews</h2>
+          <div className='row'>
             {reviews.map((review, index) => {
               return (
                 <Review
