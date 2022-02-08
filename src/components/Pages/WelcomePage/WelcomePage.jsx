3373e0e8c2f60a1a8e8f925d369cc57ed41246e3
WaleedShalal <waleed.elbana2011@gmail.com>
2022-02-08 13:15:39 +0200

    delete unneeded files

--- src/components/Pages/WelcomePage/WelcomePage.jsx
+++ src/components/Pages/WelcomePage/WelcomePage.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './WelcomePage.scss';
+import React from "react";
+import { Link } from "react-router-dom";
+import "./WelcomePage.scss";
 
 function WelcomePage() {
   return (
