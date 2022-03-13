95935e1cd527a2f18e42a634047736e455487e6f
WaleedShalal <waleed.elbana2011@gmail.com>
2022-03-13 23:10:38 +0200

    update firebaseConfig

--- src/Firebase/Firebase.js
+++ src/Firebase/Firebase.js
@@ -5,12 +5,13 @@ import "firebase/compat/storage";
 import { createContext } from "react";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBsrfymXx59nKlgmXNTl5XxpsAgEqWt0cs",
-  authDomain: "in-a-minute-fa5fc.firebaseapp.com",
-  projectId: "in-a-minute-fa5fc",
-  storageBucket: "in-a-minute-fa5fc.appspot.com",
-  messagingSenderId: "397274643372",
-  appId: "1:397274643372:web:5ec5efcaaaf62f05ce1809",
+  apiKey: "AIzaSyDEV7VD8bxklB0UZdMofTIohiaDfgIQ1jc",
+  authDomain: "in-a-minute-a51b9.firebaseapp.com",
+  projectId: "in-a-minute-a51b9",
+  storageBucket: "in-a-minute-a51b9.appspot.com",
+  messagingSenderId: "297137913239",
+  appId: "1:297137913239:web:08f79e5c4947236bacd8d2",
+  measurementId: "G-QZQFC91BC2",
 };
 firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
